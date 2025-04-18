a451cf837510917276e3a7fe11dc5477b391772b
Bharath Ashapu <bashapu1@student.gsu.edu>
2025-04-18 19:23:03 -0400

    Add Home Page and Drawer

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'home_page.dart';
 import 'login_screen.dart';
 
 class SplashScreen extends StatefulWidget {
